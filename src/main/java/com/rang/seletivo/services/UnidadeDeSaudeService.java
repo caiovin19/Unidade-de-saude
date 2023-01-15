a14317a7c00a589d2b8dd295d2e3757be2039208
Caio Vinicius <101989158+caiovin19@users.noreply.github.com>
2023-01-15 10:07:56 -0300

    Aplicado tratamento de excecoes no service

--- src/main/java/com/rang/seletivo/services/UnidadeDeSaudeService.java
+++ src/main/java/com/rang/seletivo/services/UnidadeDeSaudeService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import com.rang.seletivo.entities.UnidadeDeSaude;
 import com.rang.seletivo.repositories.UnidadeDeSaudeRepository;
+import com.rang.seletivo.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UnidadeDeSaudeService {
@@ -18,21 +23,36 @@ public class UnidadeDeSaudeService {
 	private UnidadeDeSaudeRepository repository;
 	
 	public List<UnidadeDeSaude> findAll(){
-		return repository.findAll();
+		try{
+			return repository.findAll();
+		}catch(Exception e) {
+			e.getMessage();
+		}
+		return null;	
 	}
 	
 	public UnidadeDeSaude insert(UnidadeDeSaude obj) {
-		return repository.save(obj);
+			return repository.save(obj);
+		
 	}
 	
 	public void delete(UnidadeDeSaude entity) {
-		repository.delete(entity);
+		try{
+			repository.delete(entity);
+		}catch(Exception e) {
+			throw new ResourceNotFoundException(getClass());
+		}
 	
 	}
 	
 	public UnidadeDeSaude findByCnes(String cnes) {
+		try {
 		UnidadeDeSaude entity=repository.findByCnes(cnes);
 		return entity;
+		}catch(Exception e) {
+			throw new ResourceNotFoundException(cnes);
+		}
+		
 	}
 	
 	
@@ -43,12 +63,22 @@ public class UnidadeDeSaudeService {
 	}
 
 	private void updateData(UnidadeDeSaude entity, UnidadeDeSaude obj) {
+		try {
 		entity.setNomeDoEstabelecimento(obj.getNomeDoEstabelecimento());
 		entity.setCepInicio(obj.getCepInicio());
 		entity.setCepFim(obj.getCepFim());
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(entity);
+		}
+		
 	}
 	
-	public String findUnidadeByCep(Integer cepInformado) {
-		return repository.findUnidadeByCep(cepInformado);
+	public String findUnidadeByCep(Integer cepInformado){
+		String unidadeEncontrada=repository.findUnidadeByCep(cepInformado);
+		if(unidadeEncontrada==null) {
+			throw new ResourceNotFoundException(cepInformado);
+		}
+		return unidadeEncontrada;
+		  
 	}
 }
